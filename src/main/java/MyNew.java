3fd6d343ad0ffbf5bbd11989d50d497ee4301bab
qulixu <635382296@qq.com>
2021-07-09 22:19:44 +0800

    第一次提交

---
+++ src/main/java/MyNew.java
@@ -0,0 +1,7 @@
+public class MyNew {
+    public static void main(String[] args) {
+        System.out.println("哇哈哈");
+        System.out.println("啦啦啦");
+        System.out.println("啦啦啦333");
+    }
+}
