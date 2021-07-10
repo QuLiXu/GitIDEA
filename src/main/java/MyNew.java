daedc4a09b93e6b16de0a68b81e2b95cf86751c7
QuLiXu <79850203+QuLiXu@users.noreply.github.com>
2021-07-10 10:47:57 +0800

    Update MyNew.java

--- src/main/java/MyNew.java
+++ src/main/java/MyNew.java
@@ -3,6 +3,6 @@ public class MyNew {
         System.out.println("哇哈哈");
         System.out.println("啦啦啦");
         System.out.println("萨达萨达");
-        
+        System.out.println("w哇大苏打我去饿的");
     }
 }
